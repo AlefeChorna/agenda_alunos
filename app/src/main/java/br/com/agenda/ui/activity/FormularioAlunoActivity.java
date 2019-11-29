90ed54319e00988f5e478261d034f6cb5521df5d
Alefe Chorna <alefechorna99@gmail.com>
2019-11-28 23:08:07 -0300

    Craido activity para formulário de novo aluno

---
+++ app/src/main/java/br/com/agenda/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,14 @@
+package br.com.agenda.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
