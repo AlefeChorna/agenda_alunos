fe1dc424829351db1b42dbe894e0fb104ea46ee2
Alefe Chorna <alefechorna99@gmail.com>
2019-11-28 22:53:17 -0300

    Initial commit

---
+++ app/src/main/java/br/com/agenda/MainActivity.java
@@ -0,0 +1,24 @@
+package br.com.agenda;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Aléfe Chorna", "Juca Bala", "Ze Trolano"));
+        ListView listaAlunos = findViewById(R.id.activity_main_lista_alunos);
+        listaAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
+    }
+}
